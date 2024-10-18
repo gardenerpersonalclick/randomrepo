c639242139e6573c9aeb916f13ec0fe03ff7f0dd
githubgardenerpersonalclick <github@gardenerpersonal.click>
2024-10-18 17:11:51 +0400

    Update test.py

--- test.py
+++ test.py
@@ -52,3 +52,17 @@ if __name__ == "__main__":
     if conn:
         # Fetch and print data
         fetch_data(conn)
+
+
+# snowflake connection
+import snowflake.connector
+conn = snowflake.connector.connect(
+    user='pruser',
+    password='prpassword',
+    account='xdijbpq-hz01456',
+    warehouse='COMPUTE_WH',
+    database='SNOWFLAKE'
+)
+cur = conn.cursor()
+cur.execute("SELECT current_version()")
+print(cur.fetchone())
